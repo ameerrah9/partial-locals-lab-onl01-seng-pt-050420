0240c9a0ba3cbae6bd958afc272b181233c5780d
ameerrah9 <mccullers.ghameerah@gmail.com>
2020-10-06 18:27:48 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -21,7 +21,9 @@ class StudentsController < ApplicationController
   end
 
   def index
-    @students = Student.search(params[:search])
+      
+    @students = Student.search(params[:query])
+    render 'index'
   end
 
   def student_params
